358ed9227a676bdabe6492c659e765e7dcf321a8
APTECH <aryaputra.pro25@gmail.com>
2025-10-05 12:09:53 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,27 @@
-self.addEventListener('notificationclick', event => {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow('https://google.com') // ubah dadi link GitHub Pages-mu
-  );
+// firebase-messaging-sw.js
+
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBcVJsqtRLkDklZ61XaFrN8LAj7F6eBM80",
+  authDomain: "notif-webku.firebaseapp.com",
+  databaseURL: "https://notif-webku-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "notif-webku",
+  storageBucket: "notif-webku.firebasestorage.app",
+  messagingSenderId: "1064332196897",
+  appId: "1:1064332196897:web:f8a4423c1a187e8a51f760"
+});
+
+const messaging = firebase.messaging();
+
+// iki bakal muncul nek notif dikirim pas browser ditutup / di-minimize
+messaging.onBackgroundMessage((payload) => {
+  console.log("Pesan background mlebu:", payload);
+  const notifTitle = payload.notification.title;
+  const notifOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon || "/icon.png"
+  };
+  self.registration.showNotification(notifTitle, notifOptions);
 });
